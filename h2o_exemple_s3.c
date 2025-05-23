4402521a75d9f25289bd9ad5f8d47f35ab243227
Jlidat <njlidat@yahoo.com>
2025-05-23 15:49:06 +0200

    templator_front.org fonctionnel, templator_s3.org se compile mais pas fini

--- h2o_exemple_s3.c
+++ h2o_exemple_s3.c
@@ -13,7 +13,7 @@ int main() {
   trexio_exit_code rc;
 
   // Open the TREXIO file in write mode with S3 backend
-  trexio_t* f = trexio_open("poc-trexio-b1/h2o_exemple.h5", 'w', TREXIO_S3, &rc);
+  trexio_t* f = trexio_open("poc-trexio-b1/h2o_exemple.trexio", 'w', TREXIO_S3, &rc);
   if (rc != TREXIO_SUCCESS) {
     fprintf(stderr, "Error on open: %s\n", trexio_string_of_error(rc));
     return -1;
