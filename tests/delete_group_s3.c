178ddcc356ef45bfba763f92650da146dfb771e9
N. Safi <nsafi@lcpqlx72.ups-tlse.fr>
2025-06-23 14:41:02 +0200

    dernières modifications => tests fonctionnels (sauf delete_group_s3 et overwrite_all_s3

---
+++ tests/delete_group_s3.c
@@ -0,0 +1,4 @@
+#define TEST_BACKEND_S3
+#define TREXIO_FILE_PREFIX "delete_group"
+#include "test_macros.h"
+#include "delete_group.c"
---
+++ tests/overwrite_all_s3.c
@@ -0,0 +1,5 @@
+#define TEST_BACKEND_S3
+#define TREXIO_FILE_PREFIX "overwrite_all"
+#include "test_macros.h"
+#include "overwrite_all.c"
+
